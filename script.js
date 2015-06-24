4b40bebc6d989411ff790e0c32d6a31396a0dfcd
Chris Gramme <cgramme36@gmail.com>
2015-06-24 19:51:36 -0400

    j

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var points=0;
 
 $(document).ready(function(){
-	$('.wrong').click(function(){
+	$('.wrong').on('click', function(){
 		$(this).css({'background':'#f00'});
 		points-=1;
 		$('.points').html("Points: "+points);
 	});
 
-	$('.right').click(function(){
+	$('.right').on('click',function(){
 		$(this).css({'background':'#0f0'});
 		points+=5;
 		$('.points').html("Points: "+points);
