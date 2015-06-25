6698d1ed2409318dd4196716206d8a424f4e70e4
Chris Gramme <cgramme36@gmail.com>
2015-06-25 19:45:11 -0400

    j

--- script.js
+++ script.js
@@ -4,17 +4,30 @@ $(document).ready(function(){
 });
 
 $(window).ready(function(){
-	$("body").pagecontainer("change","url",{changeHash:false});
 });
 
 	$('body').on('click','button.wrong', function(){
-		$(this).css({'background':'#f00'});
+		$(this).css({'background':'#f00'}).addClass('button-zoom').delay(500).queue(function(next){
+			$(this).removeClass('button-zoom').css({'background':'#111'});
+			next();
+		});
 		points-=2;
-		$('.points').html("Points: "+points);
+		$('.points').html("Points: "+points).css({'color':'#f00','background':'#fff'}).addClass('button-zoom').delay(500).queue(function(next){
+			$(this).removeClass('button-zoom').css({'color':'#fff','background':'#222'});
+			next();
+		});;
 	});
 
 	$('body').on('click','button.right', function(){
-		$(this).css({'background':'#0f0'});
+		$(this).css({'background':'#0f0'}).addClass('button-zoom').delay(500).queue(function(next){
+			$(this).removeClass('button-zoom').css({'background':'#111'});
+			next();
+		});
+		points-=2;
+		$('.points').html("Points: "+points).css({'color':'#0f0'}).addClass('button-zoom').delay(500).queue(function(next){
+			$(this).removeClass('button-zoom').css({'color':'#fff'});
+			next();
+		});;
 		points+=5;
 		$('.points').html("Points: "+points);
 	});
