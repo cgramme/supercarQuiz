8701980af8346d40d25a87356d637678cda8d372
Chris Gramme <cgramme36@gmail.com>
2015-06-24 20:13:34 -0400

    j

--- script.js
+++ script.js
@@ -6,18 +6,16 @@ $(document).ready(function(){
 $(window).ready(function(){
 });
 
-	$('.wrong').on('click', function(){
+	$('body').on('click','button.wrong', function(){
 		$(this).css({'background':'#f00'});
 		points-=1;
 		$('.points').html("Points: "+points);
 	});
 
-	$('.right').on('click',function(){
+	$('body').on('click','button.right', function(){
 		$(this).css({'background':'#0f0'});
 		points+=5;
 		$('.points').html("Points: "+points);
 	});
 
-	$.mobile.changePage(function(){
-		alert("durp boobies");
-	});
+	
