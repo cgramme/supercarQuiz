0c971433692fe0576061c7b10569a41667a69961
Chris Gramme <cgramme36@gmail.com>
2015-07-01 19:39:19 -0400

    k

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 var points=0;
-
 $(document).ready(function(){
 });
-
 $(window).ready(function(){
 });
-
 	$('body').on('click','button.wrong', function(){
 		$(this).css({'background':'#f00'}).addClass('button-zoom').delay(500).queue(function(next){
 			$(this).removeClass('button-zoom').css({'background':'#111'});
@@ -17,7 +14,6 @@ $(window).ready(function(){
 			next();
 		});;
 	});
-
 	$('body').on('click','button.right', function(){
 		$(this).css({'background':'#0f0'}).addClass('button-zoom').delay(500).queue(function(next){
 			$(this).removeClass('button-zoom').css({'background':'#111'});
@@ -30,12 +26,6 @@ $(window).ready(function(){
 		points+=5;
 		$('.points').html("Points: "+points);
 	});
-
-
-
-
-
-
 $(document).on('pagebeforeshow',function(){
 	console.log('pagebeforeshow');
 	var page = $('body').pagecontainer( 'getActivePage' ).attr( 'id' );
@@ -44,27 +34,25 @@ $(document).on('pagebeforeshow',function(){
 	}
 	if(page == "results"){
 		$('.total-points').html("Total Points: "+points);
-
-		if(points >= 25){
-			$('.rank').html("<b>Your rank:</b><br><i>\"You live and breath supercars!\"</i>");
+		if(points >= 85){
+			$('.rank').html("<b>Your rank:</b><br><i>\"You live and breathe supercars!\"</i>");
 			$('.total-points').css({'color':'#0f0'});
-		}else if(points >= 20){
+		}else if(points >= 70){
 			$('.rank').html("<b>Your rank:</b><br><i>\"You are a true connoisseur of fine vehicles!</i>\"");
 			$('.total-points').css({'color':'#0f0'});
-		}else if(points >= 15){
-			$('.rank').html("<b>Your rank:</b><br><i>\"You really don't know much about cars do you?\"</i>");
+		}else if(points >= 50){
+			$('.rank').html("<b>Your rank:</b><br><i>\"You really don't know much about supercars do you?\"</i>");
 			$('.total-points').css({'color':'#f00'});
-		}else if(points >= 10){
+		}else if(points >= 35){
 			$('.rank').html("<b>Your rank:</b><br><i>\"You probably live in a cave.\"</i>");
 			$('.total-points').css({'color':'#f00'});
-		}else if(points >= 5){
+		}else if(points >= 20){
 			$('.rank').html("<b>Your rank:</b><br><i>\"Lets just keep this to ourselves.\"</i>");
 			$('.total-points').css({'color':'#f00'});
-		}else if(points <= 4){
+		}else if(points <= 19){
 			$('.rank').html("<b>Your rank:</b><br><i>\"Let me try and put this nicely... um, nevermind.\"<i>");
 			$('.total-points').css({'color':'#f00'});
 		}
-
 	}
 });
 
